66f9278492d5a00ba40e6ec4b552854e4a498cc9
dnimm <87993793+dnimm@users.noreply.github.com>
2021-08-18 22:28:37 +0530

    Update views.py

--- django blog/icoder/home/views.py
+++ django blog/icoder/home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse
 from home.models import Contact
 from django.contrib import messages
+from django.contrib.auth.models import User
 from blog.models import Post
 # Create your views here.
 def home(request):
@@ -40,6 +41,22 @@ def contact(request):
        
 def handleSignup(request):
     if request.method == 'POST':
+        # Get the post parameters
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+        
+        # Check for errorneous inputs
+        # 
+        # Create the user
+        myuser = User.objects.create_user(username,email,pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+        myuser.save()
+        messages.success(request,"Your iCoder account has been successfully created")
         
     else:
         return HttpResponse(' 404 - Not Found')
