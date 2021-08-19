48d2f0df9bce723b424a8cb87175e0e7c26c3af0
dnimm <87993793+dnimm@users.noreply.github.com>
2021-08-19 13:43:15 +0530

    Update models.py

--- django blog/icoder/blog/models.py
+++ django blog/icoder/blog/models.py
@@ -11,3 +11,13 @@ class Post(models.Model):
 
     def __str__(self):
         return  self.title + " by " + self.author
+ from django.contrib.auth.models import User
+from django.utils.timezone import now
+
+class BlogComment(models.Model):
+    sno= models.AutoField(primary_key=True)
+    comment=models.TextField()
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    post=models.ForeignKey(Post, on_delete=models.CASCADE)
+    parent=models.ForeignKey('self',on_delete=models.CASCADE, null=True )
+    timestamp= models.DateTimeField(default=now)
