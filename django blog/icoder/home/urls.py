91ced319debe2944f22d121f68a63591b08e869b
dnimm <87993793+dnimm@users.noreply.github.com>
2021-08-19 10:23:18 +0530

    Update urls.py

--- django blog/icoder/home/urls.py
+++ django blog/icoder/home/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('contact', views.contact, name='contact'),
     path('about', views.about, name='about'),
      path('search', views.search, name='search'),
-    path('signup', views.handleSignUp, name='handleSignUp'),
-
+    path('signup', views.handleSignup, name='handleSignup'),
+path('login', views.handleLogin, name='handleLogin'),
+    path('logout', views.handleLogout, name='handleLogout'),
 ]
