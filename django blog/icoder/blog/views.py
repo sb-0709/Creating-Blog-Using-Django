75f39deb55ddeec18c2d0adcfa2e6c2bf7cf2256
dnimm <87993793+dnimm@users.noreply.github.com>
2021-08-19 14:33:54 +0530

    Update views.py

--- django blog/icoder/blog/views.py
+++ django blog/icoder/blog/views.py
@@ -1,13 +1,31 @@
-from django.shortcuts import render, HttpResponse
-from blog.models import Post
+from django.shortcuts import render, HttpResponse, redirect
+from blog.models import Post, BlogComment
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 # Create your views here.
-def blogHome(request):
+def blogHome(request): 
     allPosts= Post.objects.all()
     context={'allPosts': allPosts}
-    return render(request, 'blog/blogHome.html',context)
-    #return HttpResponse('This is blogHome')
+    return render(request, "blog/blogHome.html", context)
 
-def blogPost(request,slug):
-    return render(request, 'blog/blogPost.html')
-    #return HttpResponse(f'This is blogPost:{slug}')
+def blogPost(request, slug): 
+    post=Post.objects.filter(slug=slug).first()
+    comments= BlogComment.objects.filter(post=post)
+    context={'post':post, 'comments': comments}
+    return render(request, "blog/blogPost.html", context)
+
+def postComment(request):
+    if request.method == "POST":
+        comment=request.POST.get('comment')
+        user=request.user
+        postSno =request.POST.get('postSno')
+        post= Post.objects.get(sno=postSno)
+        comment=BlogComment(comment= comment, user=user, post=post)
+        comment.save()
+        messages.success(request, "Your comment has been posted successfully")
+        
+    return redirect(f"/blog/{post.slug}")
+
+
+    
